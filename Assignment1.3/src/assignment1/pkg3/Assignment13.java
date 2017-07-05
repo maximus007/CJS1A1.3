36e10dde631c6d084ffb1da142419c320e9c5750
Ishan <ishanm6@gmail.com>
2017-07-05 13:22:42 +0530

    Assignment1.3
    Bitwise and & Logical and

---
+++ Assignment1.3/src/assignment1/pkg3/Assignment13.java
@@ -0,0 +1,54 @@
+
+package assignment1.pkg3;
+
+
+
+
+
+import java.util.Scanner;
+
+
+public class Assignment13 {
+
+    
+     
+    public static void main(String[] args) {
+       
+       
+        {   
+     Scanner sc=new Scanner(System.in);
+     
+     
+         System.out.println("Enter the first value");
+     
+     byte a=sc.nextByte();
+     System.out.println("Enter the second value");
+     byte b= sc.nextByte();
+     
+     System.out.println("Result bitwise and = " +(a&b));
+    }
+    
+    /*The question was not framed correctly because Logical and
+      operates on boolean*/
+        
+       
+    
+         Scanner sd=new Scanner(System.in);
+         
+        System.out.println("Enter the first value");
+        boolean c=sd.nextBoolean();
+        System.out.println("Enter the first value");
+        boolean d=sd.nextBoolean();
+        
+        
+      
+     System.out.println("Result logical and = " + (c&&d));
+     
+    
+    }
+     
+    
+             
+    }
+    
+
